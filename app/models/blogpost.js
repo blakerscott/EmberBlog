4b9a10decd9bae8a157af40dc3dccde9a7608247
Blake Scott <blakerscott@gmail.com>
2016-03-30 14:33:01 -0700

    populate json file with blog post objects

--- app/models/blogpost.js
+++ app/models/blogpost.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     date: DS.attr(),
     title: DS.attr(),
+    artist: DS.attr(),
     catagory: DS.attr(),
-    article: DS.attr()
+    article: DS.attr(),
+    image: DS.attr()
 });
